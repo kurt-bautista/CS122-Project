dab7eb4f152dadf536b254c7764de689860b4393
Kurt Bautista <kurtv13@users.noreply.github.com>
2016-04-18 20:38:15 +0800

    Update Company Database.sql
    Added employees_hourly_rate column to workdays table

--- Company Database.sql
+++ Company Database.sql
@@ -37,8 +37,11 @@ ADD FOREIGN KEY(leave_types_id) REFERENCES leave_types(id);
 CREATE TABLE workdays(id INT NOT NULL PRIMARY KEY,
 	time_in DATETIME NOT NULL,
 	time_out DATETIME NOT NULL,
-	overtime_hours INT,
+	overtime_hours INT, --total hours?
 	employees_id INT NOT NULL,
 	leaves_id INT,
 	FOREIGN KEY(employees_id) REFERENCES employees(id),
 	FOREIGN KEY(leaves_id) REFERENCES leaves(id));
+
+ALTER TABLE workdays
+ADD COLUMN employees_hourly_rate DECIMAL NOT NULL;
