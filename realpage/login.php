351454df2e7b160466c5260e47c6e33e897fd70d
Anton Suba <antonsuba@outlook.com>
2016-04-23 19:09:23 +0800

    Added functionality to login page Added dashboard

--- realpage/login.php
+++ realpage/login.php
@@ -11,8 +11,43 @@ if(isset($_POST['submit'])){
         $password = $_POST['password'];
         
         //Variables for MySQL
-        $database = "localhost";
-        $database_user = "";
-        $database_pass = "";
+        $server = 'localhost';
+        $server_user = 'root';
+        $server_pass = '';
+        $database_name = 'realpagetest';
+        
+        $db = new mysqli($server, $server_user, $server_pass, $database_name);
+        
+        if($db->connect_errno > 0){
+            die('Unable to connect to database ['. $db->connect_error.']');
+        }
+        
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+        $username = mysqli_real_escape_string($db,$username);
+        $password = mysqli_real_escape_string($db,$password);
+        
+        //MySQLi Query [Checks if username and passowrd matched]
+        $verify_login = <<<SQL
+            SELECT * FROM login 
+            WHERE username='$username' AND password='$password'
+SQL;
+
+        //If connection failed
+        if(!$result = $db->query($verify_login)){
+            die('Error retrieving information from database ['. $db->error.']');
+        }
+        
+        //There must be exactly one row
+        if($result->num_rows == 1){
+            $_SESSION['login_user'] = $username;
+            header("location: dashboard.php");
+        }
+        else{
+            $error = "Username or Password is invalid";
+        }
+        $result->free();
+        $db->close();
     } 
 }
+?>
---
+++ realpage/session.php
@@ -0,0 +1,34 @@
+<?php
+
+$server = 'localhost';
+$server_user = 'root';
+$server_pass = '';
+$database_name = 'realpagetest';
+
+$db = new mysqli($server, $server_user, $server_pass, $database_name);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database ['. $db->connect_error.']');
+}
+
+session_start();
+if(!$user_login = $_SESSION['login_user']){
+    header("location: index.php");
+}
+
+$fetch_info = <<<SQL
+    SELECT username, first_name, last_name, remaining_leaves FROM login
+    WHERE username='$user_login'
+SQL;
+
+if(!$result = $db->query($fetch_info)){
+    die('Error retrieving user information ['. $db->error.']');
+}
+
+$row = $result->fetch_assoc();
+$login_session = $row['username'];
+if(isset($login_session)){
+    $db->close();
+    //header("location: index.php");
+}
+?>
