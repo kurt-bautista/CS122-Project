24979612d832abc8dc9ccf676fb476f28558cfe8
Anton Suba <antonsuba@outlook.com>
2016-05-09 16:30:10 +0800

    Cleaned Up leaves.php

--- realpage/leaves.php
+++ realpage/leaves.php
@@ -132,11 +132,6 @@ include('leaves_functionality.php');
                 <div class="card right col s12 hoverable">
                     <div class="card-content center">
                         <span class="card-title">Pending Approval</span>
-<<<<<<< HEAD
-                        
-=======
-                        <!--Change to PHP code-->
->>>>>>> master
                         <?php if ($number_of_days < 1): ?>
                           <h1 class="">No current pending leaves</h1>
                         <?php else: ?>
@@ -151,10 +146,7 @@ include('leaves_functionality.php');
 
                     <div class="card-reveal blue-grey darken-4 white-text">
                         <span class="card-title">Approved Leave<i class="material-icons right">close</i></span>
-<<<<<<< HEAD
                         
-=======
->>>>>>> master
                         <?php if(isset($approved_leave_start_date)): ?>
                             <h1 class=" center "><?php echo $approved_leave_duration ?> Day(s)</h1>
                             <p class="apply_roboto teal-text center" style="font-size:18px"><?php echo $approved_leave_start_date." - ".$approved_leave_end_date; ?></p>
@@ -206,11 +198,7 @@ include('leaves_functionality.php');
                                 </tr>
                             </thead>
 
-<<<<<<< HEAD
-                            <tbody>
-=======
                             <tbody>                              
->>>>>>> master
                                 <?php if($leaves_count > 0): ?>
                                     <?php for($i = 0; $i < $leaves_count; $i++){ ?>
                                     <tr>
