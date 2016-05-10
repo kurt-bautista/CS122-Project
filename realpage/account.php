a894c095618c290806ffbe2c80bc2584ff50d595
Fiel Faustino <thaf.faustino@gmail.com>
2016-05-10 08:20:38 +0800

    Account page is now fully functional

--- realpage/account.php
+++ realpage/account.php
@@ -3,17 +3,17 @@ include('account_functionality.php');
 ?>
 
 <html>
-    
+
     <head>
         <title>Account</title>
-        
+
         <link href="style.css" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-        
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
-    
+
     <body class="grey lighten-5">
 
          <!--Navbar [Copy this to all pages]-->
@@ -27,65 +27,65 @@ include('account_functionality.php');
                     <li><a href="timerecord.php">Time Record</a></li>
                     <li><a href="leaves.php">Leaves</a></li>
                     <li><a href="salaryreport.php">Salary Report</a></li>
-                    
+
                     <?php if($_SESSION['employee_type'] == 'manager'){?>
                     <li><a href="manager.php">Manager</a></li>
                     <?php } ?>
-                    
+
                     <li class="active"><a href="account.php">Account</a></li>
                     <li><a href="logout.php">Logout</a></li>
-                </ul>       
+                </ul>
             </div>
         </nav>
         <!--Navbar-->
-        
+
         <div id="main" class="center_container">
-            
+
             <div class="row">
                 <!--Account Card-->
                 <div class="card col s4 center hoverable">
                     <div class="card-content">
                         <p><img src="img/employee.jpg" class="circle profile_pic"></p>
                         <span class="card-title"><?php echo($Fname); echo($Lname);?></span>
-                        
+
                         <div class="divider" style="margin-top:25px; margin-bottom:25px;"></div>
-                        
+
                         <span style="font-size:20px">
-                        <p class="apply_roboto">Username: <span class="teal-text"><?php echo($login);?></span></p>
+                        <p class="apply_roboto">Username: <span class="teal-text"><?php echo($user_login);?></span></p>
                         <p class="apply_roboto">Account Type: <span class="teal-text"><?php echo($type);?></span></p>
                         </span>
                     </div>
                 </div>
                 <!--Account Card-->
-                
+
                 <!--Change Account Settings Card-->
                 <div class="card col s7 offset-s1 hoverable">
                     <div class="card-content">
                         <form action="" method="POST">
                             <div class="input-field col s12">
                                 <i class="material-icons prefix">perm_identity</i>
-                                <input value="<?php echo($login);?>" id="username" name="username" type="text" class="validate">
+                                <input value="<?php echo($user_login);?>" id="username" name="username" type="text" class="validate">
                                 <label for="username">Username</label>
-                                
+
                                 <div class="divider" style="margin-top:20px; margin-bottom:20px;"></div>
                             </div>
 
-                            <div class="input-field col s12">    
+                            <div class="input-field col s12">
                                 <i class="material-icons prefix">vpn_key</i>
                                 <input id="current_pass" name="current_pass" type="password" class="validate">
-                                <label for="current_pass">Current Password</label>                            
+                                <label for="current_pass">Current Password</label>
                             </div>
-                            <div class="input-field col s12">    
+                            <div class="input-field col s12">
                                 <i class="material-icons prefix">vpn_key</i>
                                 <input id="new_pass" name="new_pass" type="password" class="validate">
-                                <label for="new_pass">New Password</label>                           
+                                <label for="new_pass">New Password</label>
                             </div>
-                            <div class="input-field col s12">    
+                            <div class="input-field col s12">
                                 <i class="material-icons prefix">vpn_key</i>
                                 <input id="confirm_new_pass" name="confirm_new_pass" type="password" class="validate">
-                                <label for="confirm_new_pass">Confirm New Password</label> 
-                                
-                                <div class="divider" style="margin-top:20px; margin-bottom:20px;"></div>                           
+                                <label for="confirm_new_pass">Confirm New Password</label>
+
+                                <div class="divider" style="margin-top:20px; margin-bottom:20px;"></div>
                             </div>
                             <div class="center">
                                 <span class="apply_roboto red-text"><?php echo($error);?></span>
@@ -97,20 +97,20 @@ include('account_functionality.php');
                     </div>
                 </div>
                 <!--Change Account Settings Card-->
-                
+
             </div>
-            
+
         </div>
-       
+
        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
-        
-        <script>            
+
+        <script>
             $(document).ready(function(){
-               $(".button-collapse").sideNav(); 
+               $(".button-collapse").sideNav();
             });
         </script>
-        
+
     </body>
-    
+
 </html>
