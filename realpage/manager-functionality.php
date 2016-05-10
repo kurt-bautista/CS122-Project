9a756b9a3381e1fdbdbcc2292d9efe20ee4aa55f
Essel <kurt.v@outlook.com>
2016-05-10 20:32:11 +0800

    now only shows leave requests from team members

--- realpage/manager-functionality.php
+++ realpage/manager-functionality.php
@@ -34,7 +34,7 @@ SQL;
 
 	$fetch_leave_requests = <<<SQL
 	SELECT l.id, l.start_date, l.end_date, l.duration, CONCAT(COALESCE(e.first_name, ''), ' ', COALESCE(e.last_name, '')) AS 'employee', l.leave_reason
-	FROM leaves l, employees e WHERE l.status = 'PENDING' AND l.employees_id = e.id
+	FROM leaves l, employees e WHERE l.status = 'PENDING' AND l.employees_id = e.id AND e.manager_id = '$myId'
 SQL;
 
 	if(!$result = $db->query($fetch_leave_requests))
