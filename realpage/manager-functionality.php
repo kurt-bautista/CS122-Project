c290ace3820d3787d4006cb56a801568d3a2b8eb
Fiel Faustino <thaf.faustino@gmail.com>
2016-05-10 14:47:26 +0800

    Updated manager functionality

--- realpage/manager-functionality.php
+++ realpage/manager-functionality.php
@@ -9,7 +9,7 @@
 	if($db->connect_errno > 0){
 		die('Unable to connect to database [' . $db->connect_error . ']');
 	}
-	
+
 	ini_set('session.gc_maxlifetime', 86400);
 	session_set_cookie_params(86400);
 	session_start();
@@ -21,27 +21,27 @@
 	SELECT CONCAT(COALESCE(e.first_name, ''), ' ', COALESCE(e.last_name, '')) AS "Team Member", w.time_in AS "Time In"
 	FROM employees e LEFT JOIN workdays w
 	ON w.employees_id = e.id AND DATE(w.time_in) = CURDATE()
-	WHERE e.manager_id = '$myId' 
+	WHERE e.manager_id = '$myId'
 SQL;
 
 	if(!$result = $db->query($getTeamMembers))
 	{
 		die('There was an error running the query [' . $db->error . ']');
 	}
-	
+
 	$teamMembers = $result->fetch_all(MYSQLI_ASSOC);
 	$timedIn = $result->num_rows;
-	
+
 	$fetch_leave_requests = <<<SQL
 	SELECT l.id, l.start_date, l.end_date, l.duration, CONCAT(COALESCE(e.first_name, ''), ' ', COALESCE(e.last_name, '')) AS 'employee'
 	FROM leaves l, employees e WHERE l.status = 'PENDING' AND l.employees_id = e.id
 SQL;
-	
+
 	if(!$result = $db->query($fetch_leave_requests))
 	{
 		die('There was an error running the query [' . $db->error . ']');
 	}
-	
+
 	$pendingLeaves = $result->fetch_all(MYSQLI_ASSOC);
 	/*
 	foreach($acceptedLeaveIDs as $leaveId)
@@ -62,7 +62,7 @@ SQL;
 		{
 			die('There was an error running the query [' . $db->error . ']');
 		}
-	
+
 		$contract = $result->fetch_assoc();
 		$expected_time_in = $contract['expected_time_in'];
 		$hourly_rate = $contract['hourly_rate'];
@@ -77,7 +77,7 @@ SQL;
 			$newWorkday->bind_param('ssiid', $ti, $to, $empId, $leaveId, $hourly_rate);
 			$newWorkday->execute();
 		}
-		
+
 	}
 	foreach($rejectedLeavesIDs as $leaveId)
 	{
@@ -86,20 +86,24 @@ SQL;
 		SET status = 'REJECTED'
 		WHERE id = $leaveId;
 SQL;
-		
+
 		$db->query($rejectLeave);
 	}*/
-	
-	if(isset($_POST['submit']))
+
+	if(isset($_POST['add_employee']))
 	{
+		//echo "tangina";
 		$holiday_type = 'regular'; //
+		//foreach($_POST as $key => $val){ echo $key . " = " . $val . "---"; }
 		$newEmp = $db->prepare("INSERT INTO employees(username, password, first_name, last_name, remaining_leaves, employee_type, manager_id, holiday_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-		$newEmp->bind_param('ssssisis', $_POST['username'], $_POST['password'], $_POST['first-name'], $_POST['last-name'], $_POST['allotted-leaves'], $_SESSION['employee-type'], $myId, $holiday_type);
+		$newEmp->bind_param('ssssisis', $_POST['username'], $_POST['password'], $_POST['first-name'], $_POST['last-name'], $_POST['allotted-leaves'], $_POST['employee-type'], $myId, $holiday_type);
 		$newEmp->execute();
+		//echo $newEmp->affected_rows;
 		$empId = $newEmp->insert_id;
+		echo $empId;
 		$newEmp->close();
-		$newContract = $db->prepare("INSERT INTO employee_contracts(start_date, duration, hourly_rate, employees_id, allotted_leaves) VALUES (?, ?, ?, ?, ?)");
-		$newContract->bind_param('ssdii', $_SESSION['start-date'], $_SESSION['end-date'], $_SESSION['hourly-rate'], $empId, $_SESSION['allotted-leaves']);
+		$newContract = $db->prepare("INSERT INTO employee_contracts(start_date, duration, hourly_rate, employees_id, alloted_leaves) VALUES (?, ?, ?, ?, ?)");
+		$newContract->bind_param('ssdii', date('Y-m-d', strtotime($_POST['start-date'])), date('Y-m-d', strtotime($_POST['end-date'])), $_POST['hourly-rate'], $empId, $_POST['allotted-leaves']);
 		$newContract->execute();
 	}
 ?>
